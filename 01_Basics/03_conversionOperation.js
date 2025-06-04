74551c81ef0f17fe4ecc9fc460e9083a71b232f9
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-04 18:38:09 +0000

    opeartion discussed

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = 33
-console.log(typeof score);
+// console.log(typeof score);
 let scoore = "33"
-console.log(typeof (scoore));
+// console.log(typeof (scoore));
 
 let valueNumber = Number(scoore)
-console.log(typeof valueNumber);
-console.log(valueNumber);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
 
-console.log(Number("33abc")) // NaN
+// console.log(Number("33abc")) // NaN
 // same with undefined, string
 
 /*
@@ -20,10 +20,44 @@ console.log(Number("33abc")) // NaN
 
 let isLogged = 1;
 let isBooleanLooged = Boolean(isLogged);
-console.log(isBooleanLooged);
+// console.log(isBooleanLooged);
 
 let no = 999
-console.log(String(no));
+// console.log(String(no));
 let stringNO = String(no)
-console.log(typeof stringNO);
+// console.log(typeof stringNO);
+
+// ***************** Operations *****************
+
+let value = 4
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2**3); // 2^3 = 8
+
+let str1 = "hello"
+let str2 = "aman"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + "2"); // 12
+// console.log("1" + 2 + 2); // 122 not 14
+// console.log("1" + (2 + 2)); // now 14
+// console.log(1 + 2 + "2"); // 32 not 5
+// console.log((1 + 2) + "2"); // now still 32
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 8*5;
+
+console.table([num1, num2, num3]);
+
+let gameCounter = 100;
+++gameCounter
+console.log(gameCounter);
+
 
