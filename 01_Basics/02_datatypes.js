adef8e4694ec6a16cf9aa3d602897987c8dd75cb
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-04 18:07:20 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello")
+// alert(3 + 3) // we are using nodejs, not browser
+console.log(3+3);
+console.log("Ajay")
+
+let name = "jaya"
+let age = 15
+let isLoggedIn = false;
+let state;
+
+// number - 2 to power 53
+// bigint 
+// string
+// boolean
+// null - standalone value
+// undefined - 
+// symbol - unique
+
+// object
+
+console.log(typeof "ajay");
+console.log(typeof age);  
+console.log(typeof null);  // object
+console.log(typeof undefined);  
