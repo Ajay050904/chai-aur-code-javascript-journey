c24af12fad3026eccaa76f8d6153022d4d67ac74
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-06 17:54:14 +0000

    strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -5,7 +5,30 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String("PUBG");
+const gameName = new String("PubG");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('G'));
+
+const newString = gameName.substring(1, 3);
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 3)
+// console.log(anotherString);
+
+const newStringOne = "     AjayVish    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+
+
+
 
 
 
