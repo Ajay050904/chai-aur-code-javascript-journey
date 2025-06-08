6bad10cf0cbf4fed250c67ec1c8cf2d09b95236f
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-08 18:12:04 +0000

    dates

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,30 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2025, 5, 8);
+// console.log(myCreateDate.toDateString());
+
+// let myCreateDate = new Date(2025, 5, 8, 5, 3);
+// console.log(myCreateDate.toLocaleString());
+
+let myCreateDate = new Date("2025-05-09");
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getMonth()+ 1);
+
+
