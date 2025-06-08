5beaf68c9fc3b321dc6d84b6ad9c518def9de3be
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-08 16:35:33 +0000

    number and maths

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,17 @@
+const score = 400;
+console.log(score, typeof score);
+
+const balance = new Number(500);
+console.log(balance, typeof balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8956;
+console.log(otherNumber.toPrecision(4));
+
+const hunderds = 100000
+console.log(hunderds.toLocaleString('en-IN'));
+
+
+
