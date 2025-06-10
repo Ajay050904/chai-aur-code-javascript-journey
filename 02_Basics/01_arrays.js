8b224add2a02a9439871d3334403299390a58bcf
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-10 17:54:16 +0000

    arrays and objects

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -32,16 +32,17 @@ const newArr = myArr.join();
 
 // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3); // (1, 3]
 
-console.log(myn1);
-console.log("B ", myArr);
+// console.log(myn1);
+// console.log("B ", myArr);
 
-const myn2 = myArr.splice(1, 3);
-console.log("C ", myArr);
+const myn2 = myArr.splice(1, 3); // removes original upto (1, 3)
+// console.log("C ", myArr);
 
-console.log(myn2);
+// console.log(myn2);
 
+console.log(myArr, myArr2);
 
