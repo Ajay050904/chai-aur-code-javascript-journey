b0c4cca4f4810e0c60a370baacb88c72f333d34b
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-14 17:31:59 +0000

    functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,6 +1,27 @@
 function sayMyName() {
-   console.log("AJaY");
+  console.log("AJaY");
 }
-sayMyName();
-sayMyName();
-sayMyName();
+// sayMyName();
+// sayMyName();
+// sayMyName();
+
+function addTwoNumbers(a, b) {
+  console.log(a + b);
+}
+// addTwoNumbers(3, 4); // 7
+// addTwoNumbers(3, "4"); // 34
+// addTwoNumbers(3, "a"); // 3a
+// addTwoNumbers(3, null); // 3
+
+function addTwoNumber(a, b) {
+//   let result = a + b;
+//   return result;
+    return a + b;
+}
+const ans = addTwoNumber(5, 8);
+// console.log(ans);
+
+function loginUserMessage(userName) {
+    return `${userName} just logged in`;
+}
+console.log(loginUserMessage("Naman"));
