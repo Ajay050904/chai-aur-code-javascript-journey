1afec4860f3a186fb9896cba806e0d039bdeda77
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-16 18:13:40 +0000

    scope arrow and this

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -1,7 +1,49 @@
 const user = {
-    username : "Ajay",
-    price : 999,
-    welcomeMessage : function() {
-        console.log(`${this.username},Welcome to website`);
-    }
-}
+  username: "Ajay",
+  price: 999,
+  welcomeMessage: function () {
+    console.log(`${this.username}, Welcome to website`);
+    // console.log(this);
+  },
+};
+
+// user.welcomeMessage();
+// user.username = "Sam"
+// user.welcomeMessage();
+
+// console.log(this);
+// browser me global object - window object hota hai yha pe empty {} object
+
+// function chai() {
+//   let username = "Ajay"
+//   console.log(this.username);
+// }
+// chai() // undefined - because we can't use 'this' in the function
+
+// const chai = function () {
+//   let username = "Ajay"
+//   console.log(this.username);
+// }
+// chai() // undefined - as well because we can't use 'this' in the function
+
+// const chai = ()=> {
+//   let username = "Ajay"
+//   console.log(this.username);
+// }
+// chai() // undefined
+
+// Arrow functions
+
+// const addTwo = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(addTwo(4, 5));
+
+// implicit definition
+const addTwo = (num1, num2) => num1 + num2;
+// const addTwo = (num1, num2) => (num1 + num2); // by using () - to return object
+// const addTwo = (num1, num2) => ({username : "SAM"}); // without () undefined dega
+// console.log(addTwo(4, 9));
+
+// const myArr = [1, 5, 5, 7]
+// myArr.forEach(()=>{})
