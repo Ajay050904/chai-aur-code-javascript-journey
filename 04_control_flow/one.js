16559ada3ae6a95cbeaed59c9e55cccbade2db25
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-18 17:35:25 +0000

    rat

---
+++ 04_control_flow/one.js
@@ -0,0 +1,15 @@
+// if
+
+// if(true) { }
+// if(false) { }
+
+const isUserLoggedIn = true;
+if(isUserLoggedIn) {
+
+}
+
+if(2 === "2") { // === checks data type as well with value
+    console.log("yes");
+} else {
+    console.log("no");  
+}
