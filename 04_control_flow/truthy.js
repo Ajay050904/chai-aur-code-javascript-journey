7b0305b77f1ac95b10eef559ea761ceaaa7981a2
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-18 18:01:36 +0000

    rat4

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,8 @@
+const userEmail ="ram@123gmail.com";
+if(userEmail) {
+    console.log("GOt user email");
+} else {
+    console.log("Don;t have user email");
+}
+
+// false values -> false, 0, -0, BigInt 0n, "", null, undefined, NaN 
