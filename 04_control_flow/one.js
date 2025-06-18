5ca604f59533e8fa785907baa716f2b1215d91c7
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-18 17:46:48 +0000

    rat2

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -4,12 +4,40 @@
 // if(false) { }
 
 const isUserLoggedIn = true;
-if(isUserLoggedIn) {
-
+if (isUserLoggedIn) {
 }
 
-if(2 === "2") { // === checks data type as well with value
-    console.log("yes");
+if (2 === "2") {
+  // === checks data type as well with value
+  // console.log("yes");
 } else {
-    console.log("no");  
+  // console.log("no");
 }
+
+const tempratur = 42;
+if (tempratur < 50) {
+  // console.log("less than 50");
+} else {
+  // console.log("grater than 50");
+}
+
+const score = 100;
+if (score > 50) {
+  let power = "fly";
+  // console.log(`user power:  ${power}`);
+}
+
+const balance = 1000;
+// if(balance > 500) console.log("test");
+// else console.log("No test");
+
+// if (balance < 500) {
+//   console.log("less than 500");
+// } else if (balance < 750) {
+//   console.log("less than 750");
+// } else if (balance < 900) {
+//   console.log("less than 900");
+// } else {
+//   console.log("less than 1200");
+// }
+
