684d56929d23f06009ef70f56e179845b245d205
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-18 18:11:26 +0000

    rat 5

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -6,3 +6,31 @@ if(userEmail) {
 }
 
 // false values -> false, 0, -0, BigInt 0n, "", null, undefined, NaN 
+
+// truthy values -> true, 1, "0", 'false', " ", [], {}, function(){} 
+
+// const userEmail = []
+// if(userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+// if(Object.keys(emptyObj).length === 0) {
+//     console.log("object is empty");
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false 
+const iceTeaPRice = 100;
+iceTeaPRice <= 80 ? console.log("Less than 80") : console.log("more than 80");
+
