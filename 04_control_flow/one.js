ccd2401a5e657cf8f6d593795e7ef1f39576df7f
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-18 17:55:41 +0000

    switch

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -41,3 +41,13 @@ const balance = 1000;
 //   console.log("less than 1200");
 // }
 
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+if(userLoggedIn && debitCard) {
+    console.log("Allow to buy course");  
+} 
+if(loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
