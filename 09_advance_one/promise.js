4333ff6e92ec1490641faa68365daa9cd6abbf71
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-30 17:28:18 +0000

    promise 2

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -1,10 +1,30 @@
-const promiseOne = new Promise(function (resolve, rejecet) {
+const promiseOne = new Promise(function (resolve, reject) {
   // Do an async task
   // DB calls, cryptography, network
   setTimeout(() => {
     console.log("Async task is complete");
+    resolve(); // now connected with then()
   }, 1000);
 });
+
 promiseOne.then(function () {
   console.log("promise consumed");
+});
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(() => {
+  console.log("Async 2 resolved");
+});
+
+const promiseThree = new Promise((resolve, reject) => {
+  setTimeout(function () {
+    resolve({ username: "Chai", email: "AJay@34gmail.com" }); // normally we pass object
+  }, 1000);
+});
+promiseThree.then(function (user) {
+  console.log(user);
 });
