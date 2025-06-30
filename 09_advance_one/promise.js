3da923673eb44a74f9147af0f31b63f5cd85862e
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-30 17:39:18 +0000

    promise 3

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -25,6 +25,33 @@ const promiseThree = new Promise((resolve, reject) => {
     resolve({ username: "Chai", email: "AJay@34gmail.com" }); // normally we pass object
   }, 1000);
 });
+
 promiseThree.then(function (user) {
   console.log(user);
 });
+
+const promiseFour = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = true; // if true we will get username in then()
+    if (!error) {
+      resolve({ username: "jaya", password: "jay123" });
+    } else {
+      reject("ERROR: something went wrong");
+    }
+  }, 1000);
+});
+
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username; // can't hold it in any var
+  })
+  .then((username) => {
+    console.log(username); // do error = true to get username
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log("Finally: The promise is resolved or rejected");
+  });
