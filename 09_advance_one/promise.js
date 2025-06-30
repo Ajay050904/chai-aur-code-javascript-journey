f4505ebd10ee3f817adf9b508cfdfa8dcbf476b6
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-06-30 17:50:19 +0000

    promise test

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -55,3 +55,28 @@ promiseFour
   .finally(() => {
     console.log("Finally: The promise is resolved or rejected");
   });
+
+const promiseFive = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      resolve({ username: "javascript", password: "js321" });
+    } else {
+      reject("ERROR: JS went wrong");
+    }
+  }, 1000);
+});
+
+// another approact to accept
+// async await
+// used in DB connection
+async function consumePromise() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+consumePromise();
