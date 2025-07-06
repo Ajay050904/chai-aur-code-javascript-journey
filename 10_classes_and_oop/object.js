a5ab382145318e8af2d6869df370bec62af656a2
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-07-06 17:51:48 +0000

    2

--- 10_classes_and_oop/object.js
+++ 10_classes_and_oop/object.js
@@ -1,11 +1,26 @@
 function mulByFive(num) {
-    return num*5;
+  return num * 5;
 }
-mulByFive.power = 2
+mulByFive.power = 2;
 console.log(mulByFive(5));
 console.log(mulByFive.power);
 console.log(mulByFive.prototype);
 
 function createUser(username, score) {
-    
+  this.username = username;
+  this.score = score;
 }
+createUser.prototype.increment = function () {
+  this.score++;
+};
+const chai = new createUser("Chai", 25);
+const tea = new createUser("Tea", 250);
+createUser.prototype.printMe = function () {
+  console.log(`score is ${this.score}`);
+};
+chai.printMe();
+chai.increment();
+chai.printMe();
+tea.printMe();
+tea.increment();
+tea.printMe();
