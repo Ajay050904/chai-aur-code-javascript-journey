3ed4d579d5ea1e0dfe0396254032be42385afdef
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-07-06 18:09:12 +0000

    4

---
+++ 10_classes_and_oop/prototype.js
@@ -0,0 +1,17 @@
+let myName = "Naman     ";
+let myChannel = "NaYT     ";
+
+
+// console.log(myName.trueLength); // trim().length
+
+let myHeros = ["Thor", "SpiderMan"];
+let heroPower = {
+    thor: "Hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function() {
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+ 
