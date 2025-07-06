7c2073d6250c94681a2714ac9dc2b2c5f6156acb
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-07-06 18:15:18 +0000

    5

--- 10_classes_and_oop/prototype.js
+++ 10_classes_and_oop/prototype.js
@@ -1,17 +1,27 @@
 let myName = "Naman     ";
 let myChannel = "NaYT     ";
 
-
 // console.log(myName.trueLength); // trim().length
 
 let myHeros = ["Thor", "SpiderMan"];
 let heroPower = {
-    thor: "Hammer",
-    spiderman: "sling",
+  thor: "Hammer",
+  spiderman: "sling",
 
-    getSpiderPower: function() {
-        console.log(`Spidy power is ${this.spiderman}`);
-    }
+  getSpiderPower: function () {
+    console.log(`Spidy power is ${this.spiderman}`);
+  },
+};
+Object.prototype.Naman = function () {
+  console.log(`Naman is present in all objects`);
+};
+
+// her
+// oPower.Naman();
+myHeros.Naman();
+
+Array.prototype.heyNaman = function() {
+    console.log('Naman says heloo');
 }
-
- 
+myHeros.heyNaman();
+// heroPower.heyNaman(); // it will not get access
