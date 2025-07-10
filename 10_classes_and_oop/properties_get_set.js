e8637dcf32af44f89abb7c14702180d7199b5849
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-07-10 17:47:44 +0000

    new way of getter setter

---
+++ 10_classes_and_oop/properties_get_set.js
@@ -0,0 +1,27 @@
+// Modern way of getter and setter
+
+function User(email, password) {
+  this._email = email;
+  this._password = password;
+
+  Object.defineProperty(this, "email", {
+    get: function () {
+      return this._email.toUpperCase();
+    },
+    set: function (value) {
+      this._email = value;
+    },
+  });
+  Object.defineProperty(this, "password", {
+    get: function () {
+      return this._password.toUpperCase();
+    },
+    set: function (value) {
+      this._password = value;
+    },
+  });
+}
+
+const user1 = new User("ram@gmail.com", "ram456");
+console.log(user1.email);
+console.log(user1.password);
